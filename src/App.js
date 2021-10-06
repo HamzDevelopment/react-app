a567c24a58b027c392195ce3badc0e769e16a94d
HAMIZ ARIF - STUDENT <G00379404@gmit.ie>
2021-10-06 13:20:31 +0100

    React Lab 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Imports For Routing
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+// Imports For Styling
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+
+// Imports For Components
+import Header from './components/header';
+import Footer from './components/footer';
+import Content from './components/content';
+
+class App extends Component 
+{
+  render() {
+    return (
+      <Router>
+      <div>
+        <Navbar bg="primary" variant="dark">
+          <Container>
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/footer">Footer</Nav.Link>
+            <Nav.Link href="/header">Header</Nav.Link>
+          </Nav>
+          </Container>
+        </Navbar>
+
+        <Switch>
+          <Route path="/" exact><Content></Content></Route>
+          <Route path="/header" exact><Header></Header></Route>
+          <Route path="/footer" exact><Footer></Footer></Route>
+        </Switch>
+      </div>
+      </Router>
+    );
+  };
 }
 
 export default App;
---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Footer extends Component
+{
+    render() {
+        return(
+            <div>
+                <h1>My Footer is another component</h1>
+            </div>
+        );
+    }
+}
+
+export default Footer;
